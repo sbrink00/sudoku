e02dfea81766d9baed2ff05d679214fa997e883c
sbrink00 <sbrink00@stuy.edu>
2019-10-18 12:33:35 -0400

    updated cell class to make reading in the file easier. almost able to read in and digest sudoku board

--- cell.py
+++ cell.py
@@ -1,10 +1,13 @@
 class cell:
 
-  def __init__(self):
-    self.given = False
-    self.defAnswer = 0
+  def __init__(self, gave = False, value = 0):
+    self.given = gave
+    self.defAnswer = value
     self.prov = 0
     self.possible = []
     for i in range(1, 10): self.possible.append(i)
+
+  def toString(self):
+    return self.value
 
 c = cell()
--- readFile.py
+++ readFile.py
@@ -1,14 +1,32 @@
 import sys
+from cell import cell
 
 board = []
 
-def getAry():
+def setupAry():
   file = open(sys.argv[1], "r")
   ary = file.readlines()
-  for i in range(ary):
+  idx = 0
+  for i in range(len(ary)):
     if ary[i] == sys.argv[3]: idx = i
   nums = []
-  for i in range(idx + 1, idx + 10): nums.append(ary[idx].split(","))
+  for i in range(idx + 1, idx + 10): nums.append(ary[i].split(","))
   for i in range(len(nums)):
     for x in range(9):
-      board[9*i + x]
+      print(x)
+      if (nums[i][x].isdigit()): board.append(cell(True, int(nums[i][x])))
+      else: board.append(cell())
+
+def genOutput(generated = True):
+  if not generated: setupAry()
+  string = ""
+  for i in range(len(board)):
+    if board[i].defAnswer == 0: s = "_"
+    else: s = str(board[i].defAnswer)
+    if i % 9 == 1: string += s + "\n"
+    else: string += s + ","
+  string = string[:-1]
+  return string
+
+
+print(genOutput(False))
